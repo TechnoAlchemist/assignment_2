1bdff1e8086294ad14191fd0ceb4861b84ccd436
Gabe Lafontant <glafonta@gmail.com>
2015-02-25 13:16:35 -0500

    initial commit

---
+++ Daltons2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Daltons {
+
+  public static void main(String[] args) {
+
+    int joe, will, jack, averell;
+
+    Interger[] brothers = new Integer[4];
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("We are going to sort the Dalton bros according to height");
+
+    System.out.print("Joe's height: ");
+    joe = scan.nextInt();
+
+    System.out.print("William's height: ");
+    will = scan.nextInt();
+
+    System.out.print("Jack's height: ");
+    jack = scan.nextInt();
+
+    System.out.print("Averell's height: ");
+    averell = scan.nextInt();
+  }
+}
