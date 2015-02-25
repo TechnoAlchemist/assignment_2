ffc1fdb2e0ddb8f4facc1e3941efbc4988dc684c
Gabe Lafontant <glafonta@gmail.com>
2015-02-25 12:26:59 -0500

    now I'm able to replace the integer with diamonds

--- ShapeDiamond.java
+++ ShapeDiamond.java
@@ -7,10 +7,20 @@ public class ShapeDiamond {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
+    int num;
+    String diamond;
 
     System.out.println("Welcome to Diamond Cutter where take integers and shape them into a diamond patter.");
-    System.out.print("Please enter a number: ")
-
     
+    System.out.print("Please enter a number: ");
+    num = scan.nextInt();
+
+    String numString = Integer.toString(num);
+
+    diamond = String.format("%" + numString.length() + "s", "").replace(' ', '*');
+
+    System.out.println(diamond);
+
+
   }
 }
