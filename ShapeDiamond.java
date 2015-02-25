5c9eb44a6d6a1d55803c56e7c8ea25ffaca4d287
Gabe Lafontant <glafonta@gmail.com>
2015-02-25 11:29:55 -0500

    initial commit

---
+++ ShapeDiamond.java
@@ -0,0 +1,16 @@
+/* Gabriel Lafontant glafonta@gmail.com */
+
+import java.util.Scanner;
+
+public class ShapeDiamond {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Welcome to Diamond Cutter where take integers and shape them into a diamond patter.");
+    System.out.print("Please enter a number: ")
+
+    
+  }
+}
