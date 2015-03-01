0c98be9e643a1ce13ec96414d3e5f9505db41d86
Gabe Lafontant <glafonta@gmail.com>
2015-03-01 13:54:32 -0500

    good attempt but let's see if I can find a more efficient way without using continue

--- PrimeMenu.java
+++ PrimeMenu.java
@@ -4,6 +4,28 @@ public class PrimeMenu {
 
   public static void main(String[] args) {
 
-    
+    System.out.print("Please enter a positive natural number: ");
+
+    Scanner scan = new Scanner(System.in);
+
+    int number = scan.nextInt();
+
+    int count;
+
+    scan.close();
+
+    for (int i = 2; i <= (number); i++) {
+      count = 0;     
+      while (number % i == 0) {
+        number /= i;
+        count++;
+      }
+
+      if (count == 0) {
+        continue;
+      }
+
+      System.out.println(i + "**" + count);
+    }
   } 
 }
