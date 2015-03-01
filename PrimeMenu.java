1321fd9da52bc0d94a2c2dde73a0c43061deeda3
Gabe Lafontant <glafonta@gmail.com>
2015-03-01 14:18:18 -0500

    final version...I like this more despite using continue.

--- PrimeMenu.java
+++ PrimeMenu.java
@@ -14,42 +14,18 @@ public class PrimeMenu {
 
     scan.close();
 
-    // Print the number of 2s that divide n
-    while (number % 2 == 0) {
-      System.out.printf("%d ", 2);
-      number = number / 2;
-    }
-
-    // n must be odd at this point. So we can skip one element
-    for (int i = 3; i <= Math.sqrt(number); i = i + 2) {
-
-      // while i divides n, print i and divide n
+    for (int i = 2; i <= (number); i++) {
+      count = 0;     
       while (number % i == 0) {
-        System.out.printf("%d ", i);
-        number = number / i;
+        number /= i;
+        count++;
       }
 
+      if (count == 0) {
+        continue;
+      }
+
+      System.out.println("The prime number is: " + i + " and the count is: " + count);
     }
-
-    // this condition is to handle the case when n is a prime number  
-    // greater than 2
-
-    if ( number > 2) {
-      System.out.printf("%d ", number);
-    }
-
-    // for (int i = 2; i <= (number); i++) {
-    //   count = 0;     
-    //   while (number % i == 0) {
-    //     number /= i;
-    //     count++;
-    //   }
-
-    //   if (count == 0) {
-    //     continue;
-    //   }
-
-    //   System.out.println(i + "**" + count);
-    // }
   } 
 }
